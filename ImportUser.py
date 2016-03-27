ed22a2ffb03af95c0919001ee6eeb7500ee1110a
The Pillaz <allanon@alaimo.org>
2016-03-27 05:41:48 +0200

    PEP8 suggestions + code beautify

--- ImportUser.py
+++ ImportUser.py
@@ -2,41 +2,69 @@
 
 __author__ = "Xanio"
 __license__ = "CC0"
-__version__ = "1.0"
+__version__ = "1.1"
 __maintainer__ = "Xanio"
 __email__ = "xanio@nemesilabs.org"
 
 import csv     # imports the csv module
-import sys      # imports the sys module
-import argparse # imports the argparse module
+import argparse  # imports the argparse module
 
-parser = argparse.ArgumentParser(
+
+def options():
+    parser = argparse.ArgumentParser(
         prog='ImportUser',
         formatter_class=argparse.RawDescriptionHelpFormatter,
-        description=''''Application for import user into user-manager
-            from a CSV File. The Application create a output script RouterOS
+        description=''''An application to import user into user-manager
+                from a CSV File. The Application creates an output script
+                RouterOS
 
-            Ex. CSV File:
-            "Login","Passowd"
-            "user1","pass1"
-            "user2","pass2"
-            ...
-            ''')
-parser.add_argument('FileCsv', help='First Argument is infile.csv')
-parser.add_argument('OutFile', help='Second Argument is outfile.rsc')
-parser.add_argument('ProfileName', help='Third Argument is Profile Name')
-args = parser.parse_args()
+                Ex. CSV File:
+                "Login","Passord"
+                "user1","pass1"
+                "user2","pass2"
+                ...
+                ''')
+    parser.add_argument('FileCsv', help='First Argument is infile.csv')
+    parser.add_argument('OutputFile', help='Second Argument is outfile.rsc')
+    parser.add_argument('ProfileName', help='Third Argument is Profile Name')
+    return parser.parse_args()
 
-filename = open(sys.argv[1], 'rb') # opens the csv file
-out_file = open(sys.argv[2],"w") # opens the rsc file
-profilename = sys.argv[3] # write ProfileName
-with filename as f:
-    reader = csv.DictReader(f)
+
+def main():
+    # parsing opzioni linea di comando
+    args = options()
     try:
-        for row in reader:
-            out_file.write("tool user-manager user add disabled=no username="+row['Login']+" password="+row['Password']+" customer=admin\n")
-            out_file.write("tool user-manager user create-and-activate-profile \""+row['Login']+"\" profile="+profilename+" customer=admin\n")
-    except csv.Error as e:
-        sys.exit('file %s, line %d: %s' % (filename, reader.line_num, e))
-filename.close()
-out_file.close()
+        with open(args.FileCsv, 'rb') as a, open(args.OutputFile, 'w') as b:
+            reader = csv.DictReader(a)
+            try:
+                for row in reader:
+                    statement1 = (  # spezzo riga lunga
+                        "tool user-manager user add disabled=no "
+                        "username=%s "
+                        "password=%s "
+                        "customer=admin"
+                        "\n"
+                        % (row['Login'], row['Password'])
+                    )
+                    statement2 = (
+                        'tool user-manager user '
+                        'create-and-activate-profile "%s" '
+                        'profile=%s '
+                        'customer=admin'
+                        '\n'
+                        % (row['Login'], args.ProfileName)
+                    )
+                    b.write(statement1)
+                    b.write(statement2)
+            except csv.Error as e:
+                exit(
+                    'file %s, line %d: %s' %
+                    (args.FileCsv, reader.line_num, e)
+                )
+    except IOError as e:
+        print e
+        exit(-1)
+
+
+if __name__ == "__main__":
+    main()
